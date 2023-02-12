0dfdb2635a2e674fdd214492670c1d063c44ffbd
Bùi Hoàng Dũng <75743715+dungad2k2@users.noreply.github.com>
2023-02-12 23:46:51 +0700

    ád
    đấ

---
+++ WpfApp3/DBclass/user.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3.DBclass
+{
+    public class user
+    {
+       public string username { get; set; }
+       public string id { get; set; }
+    }
+}
